786a7b3c91039513b7c1a45637aa9d70e4ab58f6
Carlos Bauluz <c.bauluz.04@gmail.com>
2024-11-12 12:19:42 +0100

    funciones rectangulo

--- clases-y-objetos.js
+++ clases-y-objetos.js
@@ -1,34 +1,37 @@
 class Rectangulo {
   constructor(alto, ancho) {
-    this.unidad = "m"
+
     this.alto = alto;
     this.ancho = ancho;
   }
 
   describete() {
-
-    var msg = ""
-    msg += `El rectangulo es de ${this.alto}${this.unidad} x ${this.ancho}${this.unidad}`
-    msg += `, el área es de ${this.area()}${this.unidad}²`
-
-    console.log(msg);
+    console.log(`El rectangulo es de ${this.alto} x ${this.ancho}`);
   }
 
-  area() {
-    return this.alto * this.ancho
+  crece() {
+    this.alto += 1
+    this.ancho++
+  }
+
+  decrece() {
+    this.alto -= 1
+    this.ancho--
+  }
+
+  reset() {
+    this.alto = 0
+    this.ancho = 0
   }
 }
 
-function ladoAleatorio(max = 10) {
-  return Math.floor(Math.random() * (max) + 1)
-}
+const rectangulos = [
+  new Rectangulo(4, 3),
+  new Rectangulo(5, 8),
+  new Rectangulo(6, 10),
+];
 
-var rectangulos = []
-for (var i = 10000; i >= 0; i--) {
-  rectangulos.push(
-    new Rectangulo(ladoAleatorio(), ladoAleatorio()))
-}
+rectangulos.forEach(rectangulo => rectangulo.describete())
 
-rectangulos.forEach(
-  rectangulo => rectangulo.describete()
-)
+// 1 - Para cada rectangulo, se va a calcular el area 
+// 2 - Lista de 1000 rectanguilos aleatorios de lados entre 1 y 10
